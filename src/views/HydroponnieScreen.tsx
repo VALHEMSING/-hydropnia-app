3f2e05a64a8d5d6f0989daafe0751c97220c6985
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-17 22:47:15 -0500

    fix: correciones para que de vea mejor

--- src/views/HydroponnieScreen.tsx
+++ src/views/HydroponnieScreen.tsx
@@ -1,31 +1,36 @@
+
 import { ScrollView, View, Text } from "react-native";
 import { Card, Divider, Title } from "react-native-paper";
 
 const Hydroponnie = () => {
   return (
     <ScrollView
-      className="flex-1 p-4 bg-light-bg dark:bg-dark-bg"
-      contentContainerStyle={{ paddingTop: 16 }} // Ajuste para el header
+      className="flex-1 p-8 pt-10 bg-light-bg dark:bg-dark-bg"
       keyboardShouldPersistTaps="handled"
     >
       <Card className="mb-4 rounded-lg bg-light-cards dark:bg-dark-cards">
         <Card.Content>
           <View className="items-center py-4">
-            <Title 
+            {/* Título */}
+            <Title
               style={{ fontWeight: 'bold' }}
-              className="text-light-text dark:text-dark-text text-9xl"
+              className="text-light-text dark:text-dark-text text-3xl"
             >
               Configuración WiFi
             </Title>
-            <Divider 
-              style={{ height: 1, marginVertical: 8, marginTop: 12 }} 
-              className="bg-light-divider dark:bg-dark-divider"
-            />
-            <View className="rounded-lg p-4 mt-2">
-                <Text className="my-1 text-light-text dark:text-dark-text">
-                    <Text className={`font-bold color-light-secondary dark:color-dark-secondary`}>Red:</Text>
-                    <Text className="font-bold color-light-secondary dark:color-dark-secondary">nombre</Text>
+
+            {/* Divider */}
+            <Divider className="w-1 h-1  my-2" />
+
+            <View className="w-full items-start px-2 mt-2">
+              <Text className="my-1">
+                <Text className="font-bold text-light-secondary dark:text-dark-secondary">
+                  Red: 
                 </Text>
+                <Text className="font-bold text-light-secondary dark:text-dark-secondary">
+                     Hidroponia
+                </Text>
+              </Text>
             </View>
           </View>
         </Card.Content>
@@ -34,4 +39,4 @@ const Hydroponnie = () => {
   );
 };
 
-export default Hydroponnie;
+export default Hydroponnie
