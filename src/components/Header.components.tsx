f46ed11d98d4b1627f6138b3188981cafd1259e0
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-20 17:37:21 -0500

    fix: correciones de style para el componente de header.tsx

--- src/components/Header.components.tsx
+++ src/components/Header.components.tsx
@@ -6,25 +6,23 @@ type HeaderProps = {
 };
 
 export const Header = ({ title = "Hidroponia" }: HeaderProps) => {
-  const colorScheme = useColorScheme(); 
-  
+  const colorScheme = useColorScheme();
+
   return (
-    <View 
-     className={` px-4 pt-14  flex-row justify-center items-center ${
-        colorScheme === 'dark' ? 'bg-dark-primary' : 'bg-light-primary'
-      }`}
+    <View
+      className={` px-4 pt-14  flex-row justify-center items-center rounded-bl-full rounded-br-full
+         border-2 dark:border-dark-text  ${
+           colorScheme === "dark" ? "bg-dark-primary" : "bg-light-primary"
+         }`}
     >
       <View className="flex-row items-center space-x-2">
-        <MaterialIcons 
-          name="eco" 
-          size={28} 
-          color={colorScheme === 'dark' ? "#AEF6C7" : "#C9FFE2"}
+        <MaterialIcons
+          name="eco"
+          size={28}
+          color={colorScheme === "dark" ? "#AEF6C7" : "#4b7f52"}
+          className="mb-3"
         />
-        <Text 
-          className={`text-3xl font-bold ${
-            colorScheme === 'dark' ? 'text-dark-text' : 'text-light-text'
-          }`}
-        >
+        <Text className="text-3xl font-bold mb-3 text-light-text dark:text-dark-text">
           {title}
         </Text>
       </View>
