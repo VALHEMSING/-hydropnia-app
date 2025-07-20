b58b252bff2e1eba9c52fcbdccf66d5bad173d61
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-20 17:37:52 -0500

    fix: añadiendo nuevos colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./src/components/**/*.{js,jsx,ts,tsx}", "./src/views/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.tsx",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/views/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   darkMode: "class", // ¡Importante para NativeWind!
   theme: {
@@ -10,24 +15,32 @@ module.exports = {
         light: {
           primary: "#B6F9C9",
           secondary: "#B3D8A8",
-          text: "#C9FFE2",
-          buttons: "#96E8BC",
-          cards: "#7DD181",
-          bg: "#4B7F52", // Fondo claro (opcional)
-         divider: '#e9ecef',
-
+          text: "#2C5F2D",
+          textSecondary: "#5A6E5A",
+          buttonPrimary: "#96E8BC",
+          buttonText: "#FFFFFF",
+          cards: "#FFFFFF",
+          bgCard: "#e8e8e8",
+          bg: "#F1F7F2", // Fondo claro (opcional)
+          divider: "#e9ecef",
+          title: "#4CAF50",
+          bordes: "#E0E0E0",
         },
-
 
         // Dark Mode
         dark: {
-          primary: "#5B8266",
+          primary: "#204E4A",
           secondary: "#3D8D7A",
-          text: "#AEF6C7",
-          buttons: "#3E6259",
-          bg: "#212922",
-          cards: "#294936",
-          divider: '#343a40'
+          text: "#D0F0C0",
+          textSecondary: "#A8C3A3",
+          buttonPrimary: "#81C784",
+          buttonText: "#1B2E1F",
+          bg: "#1B2E1F",
+          cards: "#2F4F2F",
+          bgCard: "#f3e6e3",
+          divider: "#343a40",
+          title: "#66BB6A",
+          borderShadows: "#2F4F2F88",
         },
       },
     },
