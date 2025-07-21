b09fc2e0940232f8e7b9b077c977838a0202fe7c
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-21 15:13:21 -0500

    Feat: añado las rutas a consumir

--- src/api/engines.service.ts
+++ src/api/engines.service.ts
@@ -1,3 +1,37 @@
+import { URL_ESP32 } from "../constants/constants"
 
 
 
+export const engineServices = {
+    handleRoot: async () => {
+        await fetch(`${URL_ESP32}/`)
+    },
+    handleLedOn: async () => {
+        await fetch(`${URL_ESP32}/on`)
+    },
+    handleLedOff: async () => {
+        await fetch(`${URL_ESP32}/off`)
+    },
+    handleSubir: async () => {
+        await fetch(`${URL_ESP32}/subir`)
+    },
+    handleBajar: async () => {
+        await fetch(`${URL_ESP32}/bajar`)
+    },
+    handleBombaOn: async () => {
+        await fetch(`${URL_ESP32}/bomba_on`)
+    },
+    handleBombaOff: async () => {
+        await fetch(`${URL_ESP32}/bomba_off`)
+    },
+    handleSecadorFrio: async () => {
+        await fetch(`${URL_ESP32}/secador_frio`,
+            {method: 'POST'}
+        )
+    },
+    handleSecadorCalor: async () => {
+        await fetch(`${URL_ESP32}/secador_calor`, {
+            method: 'POST'
+        })
+    }
+}
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,4 @@
 
 export const NAME_RED = 'hidroponico01'
 export const PASSWORD_RED = '123456789'
+export const URL_ESP32 = 'http://192.168.4.1'
